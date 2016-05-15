eb305ab9a094dd37d6fbcee830de43de9f2f21d6
Mikhail Semianiuk <quercus.mikhail@gmail.com>
2016-05-15 13:02:24 +0300

    Почищен код MainActivity и файлов ресурсов

--- app/src/main/java/com/hfad/graphagregator/MainActivity.java
+++ app/src/main/java/com/hfad/graphagregator/MainActivity.java
@@ -1,20 +1,32 @@
 package com.hfad.graphagregator;
 
 import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
+import android.os.Bundle;
+
+// Для выдвижной пенели
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+// Для слушателя выдвижной панели
+import android.view.View;
+import android.widget.AdapterView;
+
+// Для обработки щелчков выдвижной пенели
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.content.Intent;
-import android.content.res.Configuration;
-import android.os.Bundle;
-import android.support.v4.widget.DrawerLayout;
+
+// Управление состоянием выдвижной панели
 import android.support.v7.app.ActionBarDrawerToggle;
+
+// Для onConfigurationChanged()
+import android.content.res.Configuration;
+
+// Прочее
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.ShareActionProvider;
 
 public class MainActivity extends Activity {
@@ -36,36 +48,44 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Заполнение массива для выдвижной пенели
+        // simple_list_item_activated_1 - подсветка выбранного варианта
         titles = getResources().getStringArray(R.array.titles);
         drawerList = (ListView)findViewById(R.id.drawer);
         drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        //Populate the ListView
         drawerList.setAdapter(new ArrayAdapter<String>(this,
                                                        android.R.layout.simple_list_item_activated_1, titles));
         drawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        //TODO: Что это?
         //Display the correct fragment.
         if (savedInstanceState != null) {
             currentPosition = savedInstanceState.getInt("position");
             setActionBarTitle(currentPosition);
         } else {
+            // При базовом создание MainActivity отобразить первый фрагмент
             selectItem(0);
         }
-        //Create the ActionBarDrawerToggle
+
+        // Создание ActionBarDrawerToggle
         drawerToggle = new ActionBarDrawerToggle(this, drawerLayout,
                                                  R.string.open_drawer, R.string.close_drawer) {
-                //Called when a drawer has settled in a completely closed state
+                // Вызывается при переходе выдвижной панели в полностью закрытое состояние
                 @Override
                 public void onDrawerClosed(View view) {
                     super.onDrawerClosed(view);
                     invalidateOptionsMenu();
                 }
-                //Called when a drawer has settled in a completely open state.
+                // Вызывается при переходе выдвижной панели в полностью открытое состояние.
                 @Override
                 public void onDrawerOpened(View drawerView) {
                     super.onDrawerOpened(drawerView);
                     invalidateOptionsMenu();
                 }
             };
+
+        // Назначить слушателя для DrawerLayout
         drawerLayout.setDrawerListener(drawerToggle);
         getActionBar().setDisplayHomeAsUpEnabled(true);
         getActionBar().setHomeButtonEnabled(true);
@@ -93,6 +113,7 @@ public class MainActivity extends Activity {
                                                            );
     }
 
+    // Обработка щелчков в выжвижной панели
     private void selectItem(int position) {
         // update the main content by replacing fragments
         currentPosition = position;
@@ -123,7 +144,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the drawer is open, hide action items related to the content view
+        // Если выдвижная панель открыта - спрятать кнопки на панели действий
         boolean drawerOpen = drawerLayout.isDrawerOpen(drawerList);
         menu.findItem(R.id.action_share).setVisible(!drawerOpen);
         return super.onPrepareOptionsMenu(menu);
@@ -132,22 +153,25 @@ public class MainActivity extends Activity {
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
+        // Синхронизация состояние после onRestoreInstanceState
         drawerToggle.syncState();
     }
 
     @Override
+    // При изменение конфигурации - передать новые данные
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
         drawerToggle.onConfigurationChanged(newConfig);
     }
 
     @Override
+    // Исправление проблемы при переворачивание экрана
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         outState.putInt("position", currentPosition);
     }
 
+    // Корректный вывод надписи в меню действий
     private void setActionBarTitle(int position) {
         String title;
         if (position == 0) {
@@ -160,7 +184,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Прочиать файл menu_main и создать меню действия
         getMenuInflater().inflate(R.menu.menu_main, menu);
         MenuItem menuItem = menu.findItem(R.id.action_share);
         shareActionProvider = (ShareActionProvider) menuItem.getActionProvider();
@@ -168,6 +192,7 @@ public class MainActivity extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    // TODO: Похоже на код для кнопки SHARE
     private void setIntent(String text) {
         Intent intent = new Intent(Intent.ACTION_SEND);
         intent.setType("text/plain");
@@ -176,6 +201,7 @@ public class MainActivity extends Activity {
     }
 
     @Override
+    // TODO: ХЗ что это
     public boolean onOptionsItemSelected(MenuItem item) {
         if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
