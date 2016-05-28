10b0d81e02813b5bdb5e9d53c8e8f10d5d169da2
Mikhail Semianiuk <quercus.mikhail@gmail.com>
2016-05-28 17:26:39 +0300

    Исправленно листание дат в просмотре графика.

--- app/src/main/java/com/quercus/graphagregator/PieChartFragment.java
+++ app/src/main/java/com/quercus/graphagregator/PieChartFragment.java
@@ -55,21 +55,77 @@ public class PieChartFragment extends Fragment {
         // Сформируем ответ
         String answer = null;
 
+        // Название недели и месяца - будут браться для нужного языка
         String[] name_week = getActivity().getResources().getStringArray(R.array.name_week);
         String[] name_month = getActivity().getResources().getStringArray(R.array.name_month);
 
-        // TODO: Должны быть проблемы с пограничным состоянием листания даты и недели
+        // TODO: Должны быть проблемы с пограничным состоянием листания дня и недели
         switch (position_calculation_system) {
             case 0:
                 // Работаем с листанением дня
-                date.add(Calendar.DAY_OF_YEAR, day_week_mounth);
-                answer = name_week[date.get(Calendar.DAY_OF_WEEK)-1] + ", " + date.get(Calendar.DAY_OF_MONTH) + " " + name_month[date.get(Calendar.MONTH)];
+                int dateMaxDayInYear = date.getActualMaximum(Calendar.DAY_OF_YEAR);
+
+                if (date.get(Calendar.DAY_OF_YEAR) == dateMaxDayInYear && day_week_mounth == 1) {
+                    date.add(Calendar.YEAR, 1);
+                    date.set(Calendar.DAY_OF_YEAR, 1);
+                }
+                else if(date.get(Calendar.DAY_OF_YEAR) == 1 && day_week_mounth == -1){
+                    date.add(Calendar.YEAR, -1);
+
+                    // Текущий год уже сменился - новое количество дней в году может быть новым
+                    date.set(Calendar.DAY_OF_YEAR, date.getActualMaximum(Calendar.DAY_OF_YEAR));
+                }
+                else {
+                    date.add(Calendar.DAY_OF_YEAR, day_week_mounth);
+                }
+
+                // Настройка форматирования текста
+                // EX: Saturday, 3 September 2016
+                answer = name_week[date.get(Calendar.DAY_OF_WEEK)-1] + ", " + date.get(Calendar.DAY_OF_MONTH) + " " + name_month[date.get(Calendar.MONTH)] + " " + date.get(Calendar.YEAR);
+
                 break;
             case 1:
                 // Работаем с листанием недели
-                // TODO: Реализоовать переключение недели
-                date.add(Calendar.WEEK_OF_YEAR, day_week_mounth);
-                answer = date.get(Calendar.WEEK_OF_YEAR) + ", week";
+                int dateMaxWeekInYear = date.getActualMaximum(Calendar.WEEK_OF_YEAR);
+
+                if (date.get(Calendar.WEEK_OF_YEAR) == dateMaxWeekInYear && day_week_mounth == 1) {
+                    date.add(Calendar.YEAR, 1);
+                    date.set(Calendar.WEEK_OF_YEAR, 1);
+
+                    Toast toast = Toast.makeText(getActivity(), "X1", Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+                else if(date.get(Calendar.WEEK_OF_YEAR) == 1 && day_week_mounth == -1){
+                    date.add(Calendar.YEAR, -1);
+
+                    // Текущая год уже сменилась - количество недель в году может быть новым
+                    date.set(Calendar.WEEK_OF_YEAR, date.getActualMaximum(Calendar.WEEK_OF_YEAR));
+                }
+                else {
+                    date.add(Calendar.WEEK_OF_YEAR, day_week_mounth);
+                }
+
+                // EX: 1 неделя 2016
+                // EX: 2 мая - 9 мая 2016
+
+                long dateInMillins  = date.getTimeInMillis();
+
+                Calendar dateStartWeek = date;
+                dateStartWeek.setTimeInMillis(dateInMillins);
+                while (dateStartWeek.get(Calendar.DAY_OF_WEEK) != Calendar.MONDAY){
+                    dateStartWeek.add(Calendar.DAY_OF_YEAR, -1);
+                }
+
+                Calendar dateEndWeek = Calendar.getInstance();
+                dateEndWeek.setTimeInMillis(dateInMillins);
+                while (dateEndWeek.get(Calendar.DAY_OF_WEEK) != Calendar.SUNDAY){
+                    dateEndWeek.add(Calendar.DAY_OF_YEAR, 1);
+                }
+
+                //answer = date.get(Calendar.WEEK_OF_YEAR) + ", week" + date.get(Calendar.YEAR);
+                answer = dateStartWeek.get(Calendar.DAY_OF_MONTH) + " " + name_month[dateStartWeek.get(Calendar.MONTH)] + " - " +
+                        dateEndWeek.get(Calendar.DAY_OF_MONTH) + " " + name_month[dateEndWeek.get(Calendar.MONTH)] + " " + dateEndWeek.get(Calendar.YEAR);
+
                 break;
             case 2:
                 // Работаем с листанием месяца
@@ -85,10 +141,10 @@ public class PieChartFragment extends Fragment {
                 else{
                     date.add(Calendar.MONTH, day_week_mounth);
                 }
-                answer = name_month[date.get(Calendar.MONTH)] + ", " + date.get(Calendar.YEAR);
 
-
-
+                // EX: May 2016
+                answer = name_month[date.get(Calendar.MONTH)] + " " + date.get(Calendar.YEAR);
+                break;
         }
 
 
